db7a4d638f7f06df556639bd91ff807bde3f80c1
NebihaDilsebo <shegolenur@hotmail.com>
2023-09-26 08:23:19 -0700

    6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,27 +1,21 @@
-import signUpUser from './4-user-promise';
-import uploadPhoto from './5-photo-reject';
+import signUpUser from './4-all-reject';
+import uploadPhoto from './5-all-reject';
 
-async function handleProfileSignup(firstName, lastName, fileName) {
-  const userPromise = signUpUser(firstName, lastName);
-  const photoPromise = uploadPhoto(fileName);
-
+export default async function handleProfileSignup(
+  firstName,
+  lastName,
+  fileName,
+) {
+  const res = [];
   try {
-    const [userResult, photoResult] = await Promise.allSettled([userPromise, photoPromise]);
-
-    return [
-      {
-        status: userResult.status,
-        value: userResult.status === 'fulfilled' ? userResult.value : userResult.reason,
-      },
-      {
-        status: photoResult.status,
-        value: photoResult.status === 'fulfilled' ? photoResult.value : photoResult.reason,
-      },
-    ];
-  } catch (error) {
-    // Handle any unexpected errors here
-    return [];
+    const user = await signUpUser(firstName, lastName);
+    res.push({ status: 'fulfilled', value: user });
+    await uploadPhoto(fileName);
+  } catch (err) {
+    res.push({
+      status: 'rejected',
+      value: `Error: ${fileName} cannot be processed`,
+    });
   }
+  return res;
 }
-
-export default handleProfileSignup;
