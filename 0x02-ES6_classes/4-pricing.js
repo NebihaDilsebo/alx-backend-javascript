42e04abad114901b48c845f70a6f83f521aa7482
NebihaDilsebo <shegolenur@hotmail.com>
2023-09-29 09:32:21 -0700

    4-pricing.js

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,51 +1,33 @@
-import Currency from './3-currency.js';
+// eslint-disable-next-line
+import Currency from './3-currency'
 
-class Pricing {
+export default class Pricing {
   constructor(amount, currency) {
     this._amount = amount;
     this._currency = currency;
   }
 
-  // Getter for amount
+  set amount(amount) {
+    this._amount = amount;
+  }
+
   get amount() {
     return this._amount;
   }
 
-  // Setter for amount
-  set amount(newAmount) {
-    if (typeof newAmount === 'number') {
-      this._amount = newAmount;
-    } else {
-      throw new TypeError('Amount must be a number');
-    }
+  set currency(currency) {
+    this._currency = currency;
   }
 
-  // Getter for currency
   get currency() {
     return this._currency;
   }
 
-  // Setter for currency
-  set currency(newCurrency) {
-    if (newCurrency instanceof Currency) {
-      this._currency = newCurrency;
-    } else {
-      throw new TypeError('Currency must be an instance of Currency class');
-    }
-  }
-
-  // Method to display the full price
   displayFullPrice() {
-    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+    return `${this.amount} ${this.currency.displayFullCurrency()}`;
   }
 
-  // Static method to convert price
   static convertPrice(amount, conversionRate) {
-    if (typeof amount === 'number' && typeof conversionRate === 'number') {
-      return amount * conversionRate;
-    }
-    throw new TypeError('Amount and conversionRate must be numbers');
+    return amount * conversionRate;
   }
 }
-
-export default Pricing;
