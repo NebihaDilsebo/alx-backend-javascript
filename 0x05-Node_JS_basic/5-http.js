b3a7d83aaa0447032b88eace084145b89d35bd5c
NebihaDilsebo <shegolenur@hotmail.com>
2023-12-01 13:05:44 -0800

    5-http.js

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,57 +1,70 @@
 const http = require('http');
-const fs = require('fs');
+const { readFile } = require('fs');
 
-// Function to read file asynchronously
-function readFileAsync(file, callback) {
-  fs.readFile(file, 'utf8', (err, data) => {
-    if (err) {
-      callback(err, null);
-    } else {
-      callback(null, data);
-    }
+const hostname = '127.0.0.1';
+const port = 1245;
+
+function countStudents(fileName) {
+  const students = {};
+  const fields = {};
+  let length = 0;
+  return new Promise((resolve, reject) => {
+    readFile(fileName, (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        let output = '';
+        const lines = data.toString().split('\n');
+        for (let i = 0; i < lines.length; i += 1) {
+          if (lines[i]) {
+            length += 1;
+            const field = lines[i].toString().split(',');
+            if (Object.prototype.hasOwnProperty.call(students, field[3])) {
+              students[field[3]].push(field[0]);
+            } else {
+              students[field[3]] = [field[0]];
+            }
+            if (Object.prototype.hasOwnProperty.call(fields, field[3])) {
+              fields[field[3]] += 1;
+            } else {
+              fields[field[3]] = 1;
+            }
+          }
+        }
+        const l = length - 1;
+        output += `Number of students: ${l}\n`;
+        for (const [key, value] of Object.entries(fields)) {
+          if (key !== 'field') {
+            output += `Number of students in ${key}: ${value}. `;
+            output += `List: ${students[key].join(', ')}\n`;
+          }
+        }
+        resolve(output);
+      }
+    });
   });
 }
 
-// Create an HTTP server
-const app = http.createServer((req, res) => {
-  // Set the response headers
-  res.setHeader('Content-Type', 'text/plain');
-
-  // Handle different URL paths
-  if (req.url === '/') {
-    res.end('Hello Holberton School!\n');
-  } else if (req.url === '/students') {
-    // Assuming the database file is passed as a command-line argument
-    const databaseFile = process.argv[2];
-
-    // Read the content of the database file asynchronously
-    readFileAsync(databaseFile, (err, data) => {
-      if (err) {
-        res.end('Error reading the database file\n');
-      } else {
-        // Process the content and generate the response
-        const students = data.trim().split('\n').filter(Boolean);
-        const totalStudents = students.length;
-        const csStudents = students.filter((student) => student.endsWith('CS')).length;
-        const sweStudents = students.filter((student) => student.endsWith('SWE')).length;
-
-        const response = `This is the list of our students\nNumber of students: ${totalStudents}\nNumber of students in CS: ${csStudents}. List: ${students.filter((student) => student.endsWith('CS')).join(', ')}\nNumber of students in SWE: ${sweStudents}. List: ${students.filter((student) => student.endsWith('SWE')).join(', ')}\n`;
-
-        res.end(response);
-      }
+const app = http.createServer((request, response) => {
+  response.statusCode = 200;
+  response.setHeader('Content-Type', 'text/plain');
+  if (request.url === '/') {
+    response.write('Hello Holberton School!');
+    response.end();
+  }
+  if (request.url === '/students') {
+    response.write('This is the list of our students\n');
+    countStudents(process.argv[2].toString()).then((output) => {
+      const outString = output.slice(0, -1);
+      response.end(outString);
+    }).catch(() => {
+      response.statusCode = 404;
+      response.end('Cannot load the database');
     });
-  } else {
-    // Handle other paths with a 404 Not Found response
-    res.writeHead(404, { 'Content-Type': 'text/plain' });
-    res.end('404 Not Found\n');
   }
 });
 
-// Set the server to listen on port 1245
-const port = 1245;
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}/`);
+app.listen(port, hostname, () => {
 });
 
-// Export the app for external use
 module.exports = app;
